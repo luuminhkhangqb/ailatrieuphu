7cac0543f1e68840ecf0b3bf8842a4eb2020cf94
luuminhkhangqb <luuminhkhang8213@gmail.com>
2024-08-27 19:42:19 +0700

    Add files via upload

---
+++ Ai là triệu phú.py
@@ -0,0 +1,254 @@
+print("Chào mừng bạn đến với trò chơi ai là triệu phú")
+a=input("Bạn đã sẵn sàng chưa?")
+print("Vậy thì chúng ta bắt đầu thôi nào")
+print("Câu 1: (200.000 đồng) Loại vật nào sau đây không có răng?")
+print("A. Dơi")
+print("B. Mèo")
+print("C. Bồ câu")
+print("D. Ngựa")
+nguoi1=input("Đáp án của bạn là: ")
+if nguoi1 == "C":
+    print("Bạn đã trả lời đúng")
+    print("Số tiền hiện tại của bạn là 200.000 đồng")
+else:
+    print("Bạn trả lời sai rồi")
+    exit()
+print("Câu 2: (200.000 đồng) Đâu là tên biểu tượng nổi tiếng ở New York, Mỹ?")
+print("A. Tượng David")
+print("B. Tượng thần vệ nữ")
+print("C. Tượng nhân sư")
+print("D. Tượng nữ thần tự do")
+nguoi2=input("Đáp án của bạn là: ")
+if nguoi2 == "D":
+    print("Bạn đã trả lời đúng")
+    print("Số tiền hiện tại của bạn là 400.000 đồng")
+else:
+    print("Bạn trả lời sai rồi")
+    exit()
+print("Câu 3: (200.000 đồng) Nhân vật hoạt hình nào dưới đây là sư tử?")
+print("A. Simba")
+print("B. Micky")
+print("C. Doraemon")
+print("D. Donald")
+nguoi3=input("Đáp án của bạn là: ")
+if nguoi3 == "A":
+    print("Bạn đã trả lời đúng")
+    print("Số tiền hiện tại của bạn là 600.000 đồng")
+else:
+    print("Bạn trả lời sai rồi")
+    exit()
+print("Câu 4: (400.000 đồng) Đâu là vận động viên cầu lông chuyên nghiệp")
+print("A. Nguyễn Tiến Minh")
+print("B. Nguyễn Quang Hải")
+print("C. Nguyễn Thị Ánh Viên")
+print("D. Nguyễn Thị Oanh")
+nguoi4=input("Đáp án của bạn là: ")
+if nguoi4 == "A":
+    print("Bạn đã trả lời đúng")
+    print("Số tiền hiện tại của bạn là 1.000.000 đồng")
+else:
+    print("Bạn trả lời sai rồi")
+    exit()
+print("Câu 5: (1.000.000 đồng) Từ nào sau đây trái nghĩa với từ đông đức?")
+print("A. Hoang đường")
+print("B. Hoang mang")
+print("C. Hoang vắng")
+print("D. Hoang phí")
+nguoi5=input("Đáp án của bạn là: ")
+if nguoi5 == "C":
+    print("Bạn đã trả lời đúng")
+    print("Số tiền hiện tại của bạn là 2.000.000 đồng")
+else:
+    print("Bạn trả lời sai rồi")
+    exit()
+c=input("Bạn có muốn chơi tiếp không ?( có hoặc không)")
+if c == "có":
+    print("Vậy thì tiếp tục thôi nào")
+else:
+    print("Số tiền bạn nhận được là: 2.000.000 ")
+    exit()
+print("Câu 6: (1.000.000 đồng) Loại động vật biển nào sau đây không phải động vật có vú?")
+print("A. Cá voi")
+print("B. Cá heo")
+print("C. Hải cẩu")
+print("D. Cá mập")
+nguoi6=input("Đáp án của bạn là: ")
+if nguoi6 == "D":
+    print("Bạn đã trả lời đúng")
+    print("Số tiền hiện tại của bạn là 3.000.000 đồng")
+else:
+    print("Bạn trả lời sai rồi")
+    print("Số tiền bạn nhận được là: 2.000.000 ")
+    exit()
+d=input("Bạn có muốn chơi tiếp không ?( có hoặc không)")
+if d == "có":
+    print("Vậy thì tiếp tục thôi nào")
+else:
+    print("Vậy số tiền bạn nhận được là: 3.000.000 ")
+    exit()
+print("Câu 7: (3.000.000 đồng) Tình nào sau đây hiện chưa có sân bay?")
+print("A. Phú Yên")
+print("B. Hà Tĩnh")
+print("C. Quảng Bình")
+print("D. Khánh Hòa")
+nguoi7=input("Đáp án của bạn là: ")
+if nguoi7 == "B":
+    print("Bạn đã trả lời đúng")
+    print("Số tiền hiện tại của bạn là 6.000.000 đồng")
+else:
+    print("Bạn trả lời sai rồi")
+    print("Số tiền bạn nhận được là: 2.000.000 ")
+    exit()
+e=input("Bạn có muốn chơi tiếp không ?( có hoặc không)")
+if e == "có":
+    print("Vậy thì tiếp tục thôi nào")
+else:
+    print("Vậy số tiền bạn nhận được là: 6.000.000")
+    exit()
+print("Câu 8: (4.000.000 đồng) Thành phố nào trên thế giới được mệnh danh là Thành phố của những cánh buồm?")
+print("A. Sydney")
+print("B. Monaco")
+print("C. Auckland")
+print("D. Cannes")
+nguoi8=input("Đáp án của bạn là: ")
+if nguoi8 == "C":
+    print("Bạn đã trả lời đúng")
+    print("Số tiền hiện tại của bạn là 10.000.000 đồng")
+else:
+    print("Bạn trả lời sai rồi")
+    print("Số tiền bạn nhận được là: 2.000.000 ")
+    exit()
+f=input("Bạn có muốn chơi tiếp không ?( có hoặc không)")
+if f == "có":
+    print("Vậy thì tiếp tục thôi nào")
+else:
+    print("Vậy số tiền bạn nhận được là: 10.000.000")
+    exit()
+print("Câu 9: (5.000.000 đồng) Bản concerto số mấy trong tổ khúc BỐN MÙA của Vivaldi viết về mùa thu?")
+print("A. Số 1")
+print("B. Số 2")
+print("C. Số 3")
+print("D. Số 4")
+nguoi9=input("Đáp án của bạn là: ")
+if nguoi9 == "C":
+    print("Bạn đã trả lời đúng")
+    print("Số tiền hiện tại của bạn là 14.000.000 đồng")
+else:
+    print("Bạn trả lời sai rồi")
+    print("Số tiền bạn nhận được là: 2.000.000 ")
+    exit()
+g=input("Bạn có muốn chơi tiếp không ?( có hoặc không)")
+if g == "có":
+    print("Vậy thì tiếp tục thôi nào")
+else:
+    print("Vậy số tiền bạn nhận được là: 14.000.000")
+    exit()
+print("Câu 10: (8.000.000 đồng) Biểu trưng của câu lạc bộ nào ở giải bóng đá Ngoại Hạng Anh có hình ảnh nhà thờ và vòng nguyệt quế?")
+print("A. Everton")
+print("B. Southampton")
+print("C. Aston Villa")
+print("D. Brentford")
+nguoi10=input("Đáp án của bạn là: ")
+if nguoi10 == "A":
+    print("Bạn đã trả lời đúng")
+    print("Số tiền hiện tại của bạn là 22.000.000 đồng")
+else:
+    print("Bạn trả lời sai rồi")
+    print("Số tiền bạn nhận được là: 2.000.000 ")
+    exit()
+h=input("Bạn có muốn chơi tiếp không ?( có hoặc không)")
+if h == "có":
+    print("Vậy thì tiếp tục thôi nào")
+else:
+    print("Vậy số tiền bạn nhận được là: 22.000.000")
+    exit()
+print("Câu 11: (8.000.000 đồng) Từ nào dưới đây có nghĩa là nhịn ăn?")
+print("A. Ẩm thức")
+print("B. Nguyệt thực")
+print("C. Tuyệt thực")
+print("D. Hàn thực")
+nguoi11=input("Đáp án của bạn là: ")
+if nguoi11 == "C":
+    print("Bạn đã trả lời đúng")
+    print("Số tiền hiện tại của bạn là 30.000.000 đồng")
+else:
+    print("Bạn trả lời sai rồi")
+    print("Số tiền bạn nhận được là: 22.000.000 ")
+    exit()
+i=input("Bạn có muốn chơi tiếp không ?( có hoặc không)")
+if i == "có":
+    print("Vậy thì tiếp tục thôi nào")
+else:
+    print("Vậy số tiền bạn nhận được là: 30.000.000")
+    exit()
+print("Câu 12: (10.000.000 đồng) Lễ hội vì hòa bình năm 2024 được tổ chức lần đầu tiên tại địa phương nào ở nước ta?")
+print("A. Quảng Trị")
+print("B. Đà Nẵng")
+print("C. Hà Nội")
+print("D. Thừa Thiên Huế")
+nguoi12=input("Đáp án của bạn là: ")
+if nguoi12 == "A":
+    print("Bạn đã trả lời đúng")
+    print("Số tiền hiện tại của bạn là 40.000.000 đồng")
+else:
+    print("Bạn trả lời sai rồi")
+    print("Số tiền bạn nhận được là: 22.000.000 ")
+    exit()
+j=input("Bạn có muốn chơi tiếp không ?( có hoặc không)")
+if j == "có":
+    print("Vậy thì tiếp tục thôi nào")
+else:
+    print("Vậy số tiền bạn nhận được là: 40.000.000")
+    exit()
+print("Câu 13: (40.000.000 đồng) Hệ thống cổng kiểm soát xuất nhập khẩu tự động hiện chưa áp dụng tại sân bay nào sau đây?")
+print("A. Phú Bài")
+print("B. Nội Bài")
+print("C. Đà Nẵng")
+print("D. Phú Quốc")
+nguoi13=input("Đáp án của bạn là: ")
+if nguoi13 == "A":
+    print("Bạn đã trả lời đúng")
+    print("Số tiền hiện tại của bạn là 80.000.000 đồng")
+else:
+    print("Bạn trả lời sai rồi")
+    print("Số tiền bạn nhận được là: 22.000.000 ")
+    exit()
+k=input("Bạn có muốn chơi tiếp không ?( có hoặc không)")
+if k == "có":
+    print("Vậy thì tiếp tục thôi nào")
+else:
+    print("Vậy số tiền bạn nhận được là: 80.000.000")
+    exit()
+print("Câu 14: (70.000.000 đồng) Trong bài hát Em đi giữa biển vành thì biển vàng là gì?")
+print("A. Đồng hoa hướng dương")
+print("B. Đồng hoa cúc")
+print("C. Rừng hoa dã quỳ")
+print("D. Cánh đồng lúa chín")
+nguoi14=input("Đáp án của bạn là: ")
+if nguoi14 == "D":
+    print("Bạn đã trả lời đúng")
+    print("Số tiền hiện tại của bạn là 150.000.000 đồng")
+else:
+    print("Bạn trả lời sai rồi")
+    print("Số tiền bạn nhận được là: 22.000.000 ")
+    exit()
+l=input("Bạn có muốn chơi tiếp không ?( có hoặc không)")
+if l == "có":
+    print("Vậy thì tiếp tục thôi nào")
+else:
+    print("Vậy số tiền bạn nhận được là: 150.000.000")
+    exit()
+print("Câu 15: (100.000.000 đồng) Đâu không phải là một trong bốn loại cây tứ quý tượng trưng cho bốn mùa Việt Nsm?")
+print("A. Hoa mai")
+print("B. Hoa dâm bụt")
+print("C. Hoa tùng")
+print("D. Hoa cúc")
+nguoi15=input("Đáp án của bạn là: ")
+if nguoi15 == "B":
+    print("Bạn đã trả lời đúng")
+    print("Số tiền hiện tại của bạn là 250.000.000 đồng")
+else:
+    print("Bạn trả lời sai rồi")
+    print("Số tiền bạn nhận được là: 22.000.000 ")
+    exit()
+print("Cảm ơn bạn đã chơi trò chơi của tôi")
